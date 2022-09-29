31aa9dafc379a4943033fbf0563f9b87af035fa6
Benjamin <beallen529@gmail.com>
2022-09-28 22:44:05 -0500

    Let CodeBox be disabled

--- WackyArchServer/UIComponents/CodeBox.razor.js
+++ WackyArchServer/UIComponents/CodeBox.razor.js
@@ -4,8 +4,19 @@ export function setup() {
     editor = ace.edit("ace-editor");
 }
 
+export function disable() {
+    editor.setReadOnly(true);
+}
+
 export function getText() {
     return editor.getValue()
+}
+
+export function setText(text) {
+    debugger;
+    editor.setReadOnly(false);
+    editor.setValue(text);
+    editor.setReadOnly(true);
 }
 
 export function setLinePointer(line, isError) {
