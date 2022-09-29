01fc12727e182167dcf4de2dfd811b23c1d45180
Benjamin <beallen529@gmail.com>
2022-09-28 22:13:50 -0500

    Add memory viewer

---
+++ WackyArchServer/UIComponents/MemoryView.razor.js
@@ -0,0 +1,31 @@
+﻿var oldPCHilite = 0;
+var oldAddrHilite = 0;
+
+export function setPCPointer(programCounter) {
+    let cell = document.getElementById("memory-cell-" + programCounter);
+    cell.classList.add("pc-hilite");
+
+    let oldcell = document.getElementById("memory-cell-" + oldPCHilite);
+    oldcell.classList.remove("pc-hilite");
+    oldPCHilite = programCounter;
+}
+
+export function setAddrPointer(addr) {
+    let cell = document.getElementById("memory-cell-" + addr);
+    cell.classList.add("addr-hilite");
+
+    let oldcell = document.getElementById("memory-cell-" + oldAddrHilite);
+    oldcell.classList.remove("addr-hilite");
+    oldAddrHilite = addr;
+}
+
+export function editCell() {
+    let result = prompt("New cell value (Hex):", "000");
+    let parsed = parseInt(result, 16);
+    if (isNaN(parsed) == false) {
+        return parsed;
+    }
+    else {
+        return null;
+    }
+}
